8262a58c64efd9bbb1bfb69825bc06f8992bae2e
alina-kam <alinakamenets08@gmail.com>
2022-08-15 20:13:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let subExprNum = '';
+    let result = '';
+    for (let i = 0; i < expr.length - 9; i += 10){
+        subExprNum = expr.substr(i, 10);
+        let subExprLet = '';
+        for (let j = 0; j < 9; j += 2){
+            if (subExprNum.substring(j, j+2) === '10'){
+                subExprLet += '.';
+            }
+            else if(subExprNum.substring(j, j+2) === '11'){
+                subExprLet += '-';
+            }
+        }
+        result += MORSE_TABLE[subExprLet] || ' ';
+    }
+
+
+    return result;
 }
 
 module.exports = {
     decode
 }
+
+decode("0000101010000000101100101010110000000010**********00000011110000000010");
